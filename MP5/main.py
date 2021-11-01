84a9cfdcf31dcc45f141ae98cf1c71b1bd5f9163
Soroush Shahi <shahi.varnus@gmail.com>
2021-11-01 10:28:13 -0500

    non maxima

--- MP5/main.py
+++ MP5/main.py
@@ -1,26 +1,82 @@
 import numpy as np
 
 from utils import *
-import numpy
-
-from scipy.ndimage import convolve
+import math
 
 
-def get_kernel(kernel_s, sigma):
+
+def get_gaussian_kernel(kernel_s, sigma):
     assert kernel_s % 2 == 1, "Kernel size must be odd"
     row = np.linspace(-(kernel_s - 1) / 2, (kernel_s - 1) / 2, kernel_s)
     gaussian_val = np.exp(-0.5 * np.square(row) / np.square(sigma))
     kernel = np.outer(gaussian_val, gaussian_val)
     return kernel / np.sum(kernel)
 
+def get_sobel_kernel():
+    # g_x = np.asarray([[-1,0,1],[-2,0,2],[-1,0,1]], dtype=int)
+    # g_y = g_x.T
+    g_x = np.array([[1.0, 0.0, -1.0], [2.0, 0.0, -2.0], [1.0, 0.0, -1.0]])
+    g_y = np.array([[1.0, 2.0, 1.0], [0.0, 0.0, 0.0], [-1.0, -2.0, -1.0]])
+    return g_x, g_y
+
 def gaussian_smoothing(img, kernel_s, sigma):
-    kernel = get_kernel(kernel_s, sigma)
-    num_ch = img.shape[2]
+    kernel = get_gaussian_kernel(kernel_s, sigma)
+    #num_ch = img.shape[2]
     res_map = np.zeros(img.shape)
-    for ch in range(num_ch):
-        res_map[:,:,ch] = convolve(img[:,:,ch], kernel)
+    #for ch in range(num_ch):
+    res_map = convolve(img, kernel)
 
     return res_map
+
+def image_gradient(img):
+
+    g_x, g_y = get_sobel_kernel()
+
+    x_res = convolve(img, g_x)
+    y_res = convolve(img, g_y)
+
+    mag = np.sqrt(np.power(x_res, 2) + np.power(y_res, 2))
+
+    cv2.imwrite("output.jpg", mag)
+
+    #theta = np.arctan2(y_res, x_res)
+    theta = np.arctan(y_res, x_res)
+    return mag, theta
+
+def look_up_table(rad):
+    if rad >=math.pi/8 and rad<3*math.pi/8:
+        return [(1,0),(2,1)]
+    elif rad >=3*math.pi/8 and rad<5*math.pi/8:
+        return [(0,0),(2,0)]
+    elif rad >=5*math.pi/8 and rad<7*math.pi/8:
+        return [(1,0),(0,1)]
+    elif rad >=7*math.pi/8 and rad<9*math.pi/8:
+        return [(0,0),(0,2)]
+    elif rad >=9*math.pi/8 and rad<11*math.pi/8:
+        return [(0,1),(1,2)]
+    elif rad >=11*math.pi/8 and rad<13*math.pi/8:
+        return [(0,2),(2,2)]
+    elif rad >=13*math.pi/8 and rad<15*math.pi/8:
+        return [(1,2),(2,1)]
+    elif rad >=15*math.pi/8 and rad<math.pi/8:
+        return [(2,0),(2,2)]
+
+
+def non_maxima_sup(img, mag, theta):
+
+
+    cols,rows = img.shape
+
+    res = np.zeros(mag.shape)
+
+    for x in range(rows):
+        for y in range(cols):
+            indices = look_up_table(theta[y][x])
+            region = np.zeros((3,3))
+            region = mag[x:x+3][y:y+3]
+
+            #if mag[y][x] > mag[y + ]
+
 
 
 
@@ -31,9 +87,14 @@ def gaussian_smoothing(img, kernel_s, sigma):
 if __name__ == '__main__':
     images = load_test_data("./data")
 
+    img = cv2.cvtColor(images[1].astype(np.uint8), cv2.COLOR_BGR2GRAY)
+    img = gaussian_smoothing(img.copy(), 5, 1)
 
-    img = gaussian_smoothing(images[0].copy(), 3, 1)
-    cv2.imwrite("output.jpg", img)
+
+    mag, theta = image_gradient(img)
+
+
+
 
 
 
--- utils.py
+++ utils.py
@@ -22,3 +22,46 @@ def load_test_data(root):
 
     imgs = np.asarray(images, dtype=object)
     return imgs
+
+
+
+# def convolve(img, kernel):
+#     columns, rows ,_ = img.shape
+#     res = np.zeros(shape=(rows, columns))
+#
+#     for i in range(rows - 2):
+#         for j in range(columns - 2):
+#             res = np.sum(np.multiply(kernel, img[i:i + 3, j:j + 3]))  # x direction
+#
+#     return res
+
+
+def convolve(img, SE, iter = 1):
+    assert SE.shape[0] % 2 == 1 and SE.shape[1] % 2 == 1, "SE size should be odd"
+    assert SE.shape[0] == SE.shape[1], "SE size should be square"
+
+    org_y, org_x = img.shape
+    img = pad_img(img, SE).astype(np.uint8)
+
+    for i in range(iter):
+
+        start_x = int(SE.shape[0] / 2)
+        start_y = start_x
+
+        half_kernel_size = int(SE.shape[0]/2)
+
+        result = np.zeros(img.shape)
+        for y in range(0, img.shape[0]):
+            for x in range(0, img.shape[1]):
+                if x < start_x or y < start_y or x >= img.shape[1] - start_x or y >= img.shape[0] - start_y:  # taking care of edges
+                    continue
+
+                region = img[y - half_kernel_size: y + half_kernel_size + 1,
+                         x - half_kernel_size: x + half_kernel_size + 1]
+                result[y,x] = np.sum(np.multiply(SE, region))
+        img = result
+
+    return img[start_y:-start_y, start_x:-start_x]
+
+def pad_img(img, SE):
+    return np.pad(img, int(SE.shape[0]/2), mode="edge")
